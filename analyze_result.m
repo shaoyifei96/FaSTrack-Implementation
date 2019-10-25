e8ed22814f38a265d9346ad2d054cfae94b5a7a1
shaoyifei96 <shaoyifei96@gmail.com>
2019-10-25 11:36:54 -0400

    analyze crashes seperately

--- analyze_result.m
+++ analyze_result.m
@@ -12,24 +12,36 @@ RTD_time=[];
 for i = 1:100
     try
         load("simulation_summary"+num2str(i)+".mat")
-      
-                if summary(1).crash_check
-                    RTD_crashes = RTD_crashes+1
-                elseif summary(1).goal_check
+                if summary(1).goal_check
                     RTD_goal = RTD_goal+1
                     RTD_time=[RTD_time; summary(1).sim_time_vector(end)]
-                else
-                    
                 end
-        
-                if summary(2).crash_check
-                    fatrack_crashes = fatrack_crashes+1
-                elseif summary(2).goal_check
+               
+                RTD_crashes = RTD_crashes+summary(1).crash_check;
+                fatrack_crashes = fatrack_crashes+summary(2).crash_check
+
+                if summary(2).goal_check
                     fastrack_goal = fastrack_goal+1
                     fastrack_time=[fastrack_time; summary(2).sim_time_vector(end)]
-                else
-                    
                 end
+                    
+%                 if summary(1).crash_check
+%                     RTD_crashes = RTD_crashes+1
+%                 elseif summary(1).goal_check
+%                     RTD_goal = RTD_goal+1
+%                     RTD_time=[RTD_time; summary(1).sim_time_vector(end)]
+%                 else
+%                     
+%                 end
+%         
+%                 if summary(2).crash_check
+%                     fatrack_crashes = fatrack_crashes+1
+%                 elseif summary(2).goal_check
+%                     fastrack_goal = fastrack_goal+1
+%                     fastrack_time=[fastrack_time; summary(2).sim_time_vector(end)]
+%                 else
+%                     
+%                 end
         
     catch ME
         continue
