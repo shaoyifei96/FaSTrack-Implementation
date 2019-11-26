95f467a7abc0fcd651b919a6cfd85fa9ba04d12b
shaoyifei96 <shaoyifei96@gmail.com>
2019-11-26 11:40:20 -0500

    fastrack test updated

--- running_simulations/run_fastrack_test.m
+++ running_simulations/run_fastrack_test.m
@@ -1,22 +1,23 @@
 close all; clear
-load Dubin4D1.5_43_veryHigh_manydays.mat
+load Dubin4D1mpers_veryHigh.mat
 TEB= zeros(10, 1)
 spds= zeros(10, 1)
-v_maxmax = 1.5;
-n = 29;
-% for i = 1: n
-% spd = i*v_maxmax/n;
- spd = 0.45;
+v_maxmax = 1.0;
+n = 10;
+for i = 3: n
+spd = i*v_maxmax/n;
+%  spd = 0.45;
 A = fastrack_agent;
 A.LLC.TEB.sD.dynSys.v_max = spd;
-T = [0 10 20 30];
+T = [0 10 20 30 ];
 U = zeros(A.n_inputs,length(T));
-Z = [0 2 2 2 ; 0 0 0 2; 0 0 0 0; 0.2 0.2 0 0.2];
-A.move(29,T,U,Z);
-% spds(i) = spd;
-% TEB(i) = A.TEB_max;
+Z = [0 2 2 2 ; 0 0 2 2; 0 0 0 0 ; 0.2 0.2 0.2 0 ];
+A.move(25,T,U,Z);
+spds(i) = spd;
+TEB(i) = A.TEB_max;
 %%
- plot(A)
+
  
   figure(1) ; axis equal ; hold on ; grid on ;
-% end
+   plot(A)
+end
